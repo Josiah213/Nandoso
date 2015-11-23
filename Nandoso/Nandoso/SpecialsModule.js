b2c3b9bfa934f7e03d6c91bf5743731bc63e27d0
Josiah Martinez <josiahg2x@gmail.com>
2015-11-23 14:58:48 +1300

    Create table and fill with dummy data which should be replaced later with the actual data

--- Nandoso/Nandoso/SpecialsModule.js
+++ Nandoso/Nandoso/SpecialsModule.js
@@ -1,7 +1,15 @@
 ﻿var SpecialsModule = (function () {
     return {
         getSpecials: function (callback) {
-
+            $.ajax({
+                type: "GET",
+                dataType: "json",
+                url: "http://api.uinames.com/?amount=25", //need to check the url for this that gets specials
+                success: function (data) {
+                    console.log(data);
+                    callback(data);
+                }
+            });
         }
     };
 }());
--- Nandoso/Nandoso/index.js
+++ Nandoso/Nandoso/index.js
@@ -1,3 +1,39 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
-
+    loadSpecials();
 });
+
+function loadSpecials() {
+    //reference the div/table to display our data in
+    var specialsTable = document.getElementById("tblspecialscontent");
+
+    SpecialsModule.getSpecials(function (specialsList) {
+        setupSpecialsTable(specialsTable);
+    });
+
+    //callback function for data that gets returned from the SpecialsModule
+    function setupSpecialsTable(specials) {
+        console.log(specials);
+        for (i = 0; i < specials.length; i++) {
+            //create the rows
+            var row = document.createElement('tr');
+
+            //add columns to the row (td/ data cells)
+            //might have to change this later to include specials as opposed to having students etc.
+            var specialsnamecol = document.createElement('td');
+            specialsnamecol.innerHTML = specials[i].name;
+            row.appendChild(specialsnamecol);
+
+            var specialspricecol = document.createElement('td');
+            specialspricecol.innerHTML = specials[i].surname;
+            row.appendChild(specialspricecol);
+
+            var specialsdisplaycol = document.createElement('td');
+            specialsdisplaycol.innerHTML = specials[i].name;
+            row.appendChild(specialsdisplaycol);
+
+            //append the row to the end of the table
+            specialsTable.appendChild(row);
+        }
+    }
+
+}
