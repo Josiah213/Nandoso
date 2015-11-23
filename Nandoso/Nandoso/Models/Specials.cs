47e23ad0a0ff54bf54f149104fc9714ec343fceb
Josiah Martinez <josiahg2x@gmail.com>
2015-11-23 15:28:03 +1300

    Scrapped first part now working on api

---
+++ Nandoso/Nandoso/Models/Specials.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+
+namespace Nandoso.Models
+{
+    public class Specials
+    {
+        public int ID { get; set; }
+        public string SpecialName { get; set; }
+        public string SpecialPrice { get; set; }
+        public byte[] picture { get; set; }
+
+    }
+}
