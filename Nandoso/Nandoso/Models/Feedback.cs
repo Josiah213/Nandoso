5b508d73b33a9ddd5a8ae840d496b42b47d2bc40
Josiah Martinez <josiahg2x@gmail.com>
2015-11-24 17:55:07 +1300

    Added different pages that can be navigated from the nav bar

---
+++ Nandoso/Nandoso/Models/Feedback.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace Nandoso.Models
+{
+    public class Feedback
+    {
+        public int ID { get; set; }
+        public string CommentorName { get; set; }
+        public string Comment { get; set; }
+    }
+}
