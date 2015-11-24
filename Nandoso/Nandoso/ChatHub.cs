cd4a38abda6132313f406db008e53ed28504f7ad
Josiah Martinez <josiahg2x@gmail.com>
2015-11-24 22:37:14 +1300

    Added SignalR, updated some pages

---
+++ Nandoso/Nandoso/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Nandoso
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string username, string message)
+        {
+            //broadcast what we pass across the network so everyone in it will be able to see it
+            Clients.All.broadcastMessage(username, message);
+        }
+    }
+}
