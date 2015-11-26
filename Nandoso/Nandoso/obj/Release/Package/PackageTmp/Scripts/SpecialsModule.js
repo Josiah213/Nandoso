6aa3ccf013046948ae8ae7925a340c29d5ab9d68
Josiah Martinez <josiahg2x@gmail.com>
2015-11-26 19:48:53 +1300

    Fully working site and api, had to change the database when you publish

--- Nandoso/Nandoso/obj/Release/Package/PackageTmp/Scripts/SpecialsModule.js
+++ Nandoso/Nandoso/obj/Release/Package/PackageTmp/Scripts/SpecialsModule.js
@@ -7,7 +7,7 @@
             $.ajax({
                 type: "GET",
                 dataType: "json",
-                url: "https://nandosojosiahm.azurewebsites.net/api/Specials",
+                url: "https://nandosojosiahm.azurewebsites.net/api/specials",
                 success: function (data) {
                     console.log(data);
                     callback(data);
