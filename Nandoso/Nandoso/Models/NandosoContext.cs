7de68e95e92b53827a38558c364f0754c20ebb2e
Josiah Martinez <josiahg2x@gmail.com>
2015-11-26 17:01:14 +1300

    Revert to working

--- Nandoso/Nandoso/Models/NandosoContext.cs
+++ Nandoso/Nandoso/Models/NandosoContext.cs
@@ -30,30 +30,30 @@ namespace Nandoso.Models
             protected override void Seed(NandosoContext context)
             {
                 var specials = new List<Specials>
-                {
-                    new Specials { SpecialName = "Quarter Chicken", SpecialPrice = "$8.90",
-                        SpecialDescription = "Small fraction, big reaction." },
-                    new Specials { SpecialName = "Half Chicken", SpecialPrice = "$12.90",
-                        SpecialDescription = "Nothing half hearted here." },
-                    new Specials { SpecialName = "5 Grilled Tenderloins", SpecialPrice = "$8.90",
-                        SpecialDescription = "So tender you'll feel it in your loins." },
-                    new Specials { SpecialName = "Portuguese Paella", SpecialPrice = "$13.90",
-                        SpecialDescription = "Grilled chicken pieces combined with veggie strips, tossed over our spicy rice." },
-                    new Specials { SpecialName = "8 BBQ Ribs", SpecialPrice = "$11.90",
-                        SpecialDescription = "Bite sized chicken on the bone." },
-                    new Specials { SpecialName = "5 BBQ Wings", SpecialPrice = "$11.90",
-                        SpecialDescription = "Reach new heights of enjoyment." },
-                    new Specials { SpecialName = "Classic Chicken Wrap", SpecialPrice = "$9.90",
-                        SpecialDescription = "Trim, taut & terrific tenderloins basted & grilled, dressed with crisp lettuce, tomato & our special light mayo, all quietly nestled in a warm tortilla." },
-                    new Specials { SpecialName = "Chicken Breast Pita", SpecialPrice = "$10.90",
-                        SpecialDescription = "Portuguese passion in a pita with a succulent, skinless chicken breast, crisp iceberg lettuce, cucumber & juicy tomato, topped with our special light mayo." },
-                    new Specials { SpecialName = "Classic Chicken Breast Burger", SpecialPrice = "$9.90",
-                        SpecialDescription = "Sink your teeth into our soft, seeded bun filled with a delicious marinated skinless chicken breast, fresh lettuce, tomato & our special light mayo." },
-                    new Specials { SpecialName = "Supremo Chicken Breast Burger", SpecialPrice = "$12.90",
-                        SpecialDescription = "Combining a freshly baked Portuguese roll, chicken breast, gourmet lettuce, tomato, light mayo & PERi-PERi chutney." },
-                    new Specials { SpecialName = "Double Breast Burger", SpecialPrice = "$15.90",
-                        SpecialDescription = "Two marinated & basted skinless chicken breasts, accompanied by crisp lettuce, tomato & our special light mayo inside a soft, seeded bun." }
-                };
+            {
+                new Specials { SpecialName = "Quarter Chicken", SpecialPrice = "$8.90",
+                    SpecialDescription = "Small fraction, big reaction." },
+                new Specials { SpecialName = "Half Chicken", SpecialPrice = "$12.90",
+                    SpecialDescription = "Nothing half hearted here." },
+                new Specials { SpecialName = "5 Grilled Tenderloins", SpecialPrice = "$8.90",
+                    SpecialDescription = "So tender you'll feel it in your loins." },
+                new Specials { SpecialName = "Portuguese Paella", SpecialPrice = "$13.90",
+                    SpecialDescription = "Grilled chicken pieces combined with veggie strips, tossed over our spicy rice." },
+                new Specials { SpecialName = "8 BBQ Ribs", SpecialPrice = "$11.90",
+                    SpecialDescription = "Bite sized chicken on the bone." },
+                new Specials { SpecialName = "5 BBQ Wings", SpecialPrice = "$11.90",
+                    SpecialDescription = "Reach new heights of enjoyment." },
+                new Specials { SpecialName = "Classic Chicken Wrap", SpecialPrice = "$9.90",
+                    SpecialDescription = "Trim, taut & terrific tenderloins basted & grilled, dressed with crisp lettuce, tomato & our special light mayo, all quietly nestled in a warm tortilla." },
+                new Specials { SpecialName = "Chicken Breast Pita", SpecialPrice = "$10.90",
+                    SpecialDescription = "Portuguese passion in a pita with a succulent, skinless chicken breast, crisp iceberg lettuce, cucumber & juicy tomato, topped with our special light mayo." },
+                new Specials { SpecialName = "Classic Chicken Breast Burger", SpecialPrice = "$9.90",
+                    SpecialDescription = "Sink your teeth into our soft, seeded bun filled with a delicious marinated skinless chicken breast, fresh lettuce, tomato & our special light mayo." },
+                new Specials { SpecialName = "Supremo Chicken Breast Burger", SpecialPrice = "$12.90",
+                    SpecialDescription = "Combining a freshly baked Portuguese roll, chicken breast, gourmet lettuce, tomato, light mayo & PERi-PERi chutney." },
+                new Specials { SpecialName = "Double Breast Burger", SpecialPrice = "$15.90",
+                    SpecialDescription = "Two marinated & basted skinless chicken breasts, accompanied by crisp lettuce, tomato & our special light mayo inside a soft, seeded bun." }
+            };
                 specials.ForEach(s => context.Specials.AddOrUpdate(p => p.SpecialName, s));
                 context.SaveChanges();
 
@@ -62,11 +62,7 @@ namespace Nandoso.Models
                     new Feedbacks { CommentorName = "Tony",
                         Comment = "Can't get enough of those BBQ ribs, too good =P"},
                     new Feedbacks { CommentorName = "Jack - Nandoso Team",
-                        Comment = "Thanks for your feedback! I love the ribs too"},
-                    new Feedbacks { CommentorName = "Dave",
-                        Comment = "What are your closing times?"},
-                    new Feedbacks { CommentorName = "Meg - Nandoso Team",
-                        Comment = "9pm weekdays, 10pm on weekends =)"}
+                        Comment = "Thanks for your feedback! I love the ribs too"}
                 };
                 feedbacks.ForEach(s => context.Feedbacks.AddOrUpdate(p => p.CommentorName, s));
                 context.SaveChanges();
