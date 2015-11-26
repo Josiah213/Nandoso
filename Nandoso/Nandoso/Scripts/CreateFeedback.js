e17d150f348e7ed38723a988c1c34f3503274cc3
Josiah Martinez <josiahg2x@gmail.com>
2015-11-26 16:59:29 +1300

    cdfdsfs

---
+++ Nandoso/Nandoso/Scripts/CreateFeedback.js
@@ -0,0 +1,36 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    setupFeedbackSubmit();
+    setupReturn();
+});
+
+function setupFeedbackSubmit() {
+
+    //Creating student from form parameters
+
+    var form = document.forms.create;
+    // Need to add our own custom event for form submission
+    form.onsubmit = function (e) {
+        // ... and prevent the default action from occuring
+        e.preventDefault();
+
+        //Creating student from form parameters
+        var newFeedback = {
+            // Access the data in the fields with .value 
+            name: document.getElementById("Author").value,
+            post: document.getElementById("Comment").value
+        }
+
+        // Take me back home when done!
+        FeedbacksModule.addFeedback(newFeedback, function () {
+            window.location.href = "FeedbackPage.html";
+        });
+    }
+
+};
+
+// Add event listener, cancel button will take you back to home page
+function setupReturn() {
+    document.getElementById('btncancel').addEventListener('click', function () {
+        window.location.href = "FeedbackPage.html";
+    });
+}
---
+++ Nandoso/Nandoso/obj/Release/Package/PackageTmp/Scripts/SpecialsModule.js
@@ -0,0 +1,18 @@
+﻿var SpecialsModule = (function () {
+    // Return anything that you want to expose outside the closure
+    return {
+        getSpecials: function (callback) {
+            console.log("this was called from SpecialsModule");
+
+            $.ajax({
+                type: "GET",
+                dataType: "json",
+                url: "https://nandosojosiahm.azurewebsites.net/api/specials",
+                success: function (data) {
+                    console.log(data);
+                    callback(data);
+                }
+            });
+        }
+    };
+}());
